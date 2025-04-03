14f9b81f1d3f40c834a20a0422896ec41457de23
Cristopher Andrade <133789100+cristopheroan@users.noreply.github.com>
2025-04-03 13:55:07 +0000

    Acrescentando loops aninhados para representar o movimento do cavalo. Nivel aventureiro.

--- xadrez.c
+++ xadrez.c
@@ -7,7 +7,7 @@
 int main() {
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
- char torre = 5, bispo = 5, rainha = 8;
+ int torre = 5, bispo = 5, rainha = 8;
  int t = 1, r = 1;
     // Implementação de Movimentação do Bispo
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
@@ -29,6 +29,23 @@ while(r <= rainha) {
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
     // Um loop pode representar a movimentação horizontal e outro vertical.
+    printf("\n");
+
+    int Cavalo = 3;//varialvel representando a peça + o valor dela.
+    int c = 3;//numero de movimentos totais
+    
+    //loops aninhados utilizando "while" e o "for" para representar o movimento em "L" do cavalo.
+    while (c <= Cavalo)//Loop em "while" representando 1 movimento a esquerda.
+    {
+        for (int a = 1; a <= 2; a++)//Loop em "for" representando dois movimentos para baixo.
+        {
+            printf("Cavalo move %d: Baixo\n", a);
+        }
+        printf("Cavalo move %d: Esquerda\n", c);
+        c++;
+
+    }
+    
 
     // Nível Mestre - Funções Recursivas e Loops Aninhados
     // Sugestão: Substitua as movimentações das peças por funções recursivas.
